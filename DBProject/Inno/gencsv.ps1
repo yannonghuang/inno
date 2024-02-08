2a94311997fa5166ea6962d023474362fec1b5d4
yannong <yannonghuang@icloud.com>
2024-02-07 18:22:36 -0800

    add gencsv

---
+++ DBProject/Inno/gencsv.ps1
@@ -0,0 +1,29 @@
+$Server = "localhost" 
+$userId = "sa" 
+$passwd = "sunset@919325"
+$dbName = "master"
+
+$tables = @(
+    'adx_bom',
+    'adx_productlocation',
+    'adx_method_buy',
+    'adx_method_make',
+    'adx_transportation',
+    'adx_vendor',
+    'adx_product'
+    )
+$folder = "/Users/yannonghuang/Documents/personal/projects/SRI/Import prospects/Adexa/Innolight/PoC 2023/Adexa Feeds/csv_deduped/"
+
+$ConnectionString = "Server=$serverDN;Database=$dbName;User Id=$userId;Password=$passwd;TrustServerCertificate=True"
+
+
+
+for ($i = 0; $i -lt $tables.Length; $i++) {
+    $query = "SELECT * FROM " + $tables[$i]
+    $file = $folder + $tables[$i] + "_inno.csv"
+
+    Invoke-Sqlcmd  -ConnectionString $ConnectionString -Query $query  | 
+    Export-Csv -UseQuotes Never -NoTypeInformation -Encoding UTF8 -Path $file ","
+
+}
+
